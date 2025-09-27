83f1fd1aa4e6b1553e2c444702e98201f0b24012
ererdewubudesi <116863941+ererdewubudesi@users.noreply.github.com>
2025-09-27 07:25:33 +0000

    Deploying to gh-pages from @ ererdewubudesi/ererdewubudesi.github.io@78a7afa78cd71eb8610e0716ae2ca12fc87a0511 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -418,16 +418,13 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-to-appear-at-emnlp-2025",
+          title: 'To appear at EMNLP 2025.🥳',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
